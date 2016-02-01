dcf34587a869435e7390613249477a0506bfed09
rsgc-leder-b <bleder@rsgc.on.ca>
2016-02-01 09:47:27 -0500

    Quiz Beginning
    The basis of an iPhone quiz app

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Brendan Leder on 2016-01-28.
+//  Copyright © 2016 Brendan Leder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+    }
+    
+}
