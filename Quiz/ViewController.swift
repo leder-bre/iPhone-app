c189ecce15c3bc0082c811556ff0f393dc9631a7
rsgc-leder-b <bleder@rsgc.on.ca>
2016-02-10 18:32:46 -0500

    Added quiz and basic questions

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,12 +12,35 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = [ "From what is cognac made?",
+                                "What is 3*21-2*20?",
+                                "What is the capital of Ontario?"]
+    
+    let answers: [String] = [   "Grapes",
+                                "23",
+                                "Toronto"]
+    
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(sender: AnyObject) {
+        ++currentQuestionIndex
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
         
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
     
     @IBAction func showAnswer(sender: AnyObject) {
-        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
     }
     
 }
